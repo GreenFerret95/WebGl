b7494cac78c9633a8f16eecf1c8ac0ee43581100
Brandon Smith <bpsmith0295@gmail.com>
2019-04-29 21:18:54 -0600

    Created resources folder. Created webglUtils.js which contains helper functions. Rendered 50 randomly placed, randomly colored rectangles on the screen

---
+++ WebGL/resources/webgl-utils.js
@@ -0,0 +1,83 @@
+/**
+ * Creates and compiles a shader.
+ *
+ * @param {!WebGLRenderingContext} gl The WebGL Context.
+ * @param {string} shaderSource The GLSL source code for the shader.
+ * @param {number} shaderType The type of shader, VERTEX_SHADER or
+ *     FRAGMENT_SHADER.
+ * @return {!WebGLShader} The shader.
+ */
+function compileShader(gl, shaderSource, shaderType) {
+  // Create the shader object
+  var shader = gl.createShader(shaderType);
+ 
+  // Set the shader source code.
+  gl.shaderSource(shader, shaderSource);
+ 
+  // Compile the shader
+  gl.compileShader(shader);
+ 
+  // Check if it compiled
+  var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (!success) {
+    // Something went wrong during compilation; get the error
+    throw "could not compile shader:" + gl.getShaderInfoLog(shader);
+  }
+ 
+  return shader;
+}
+
+
+/**
+ * Creates a program from 2 shaders.
+ *
+ * @param {!WebGLRenderingContext) gl The WebGL context.
+ * @param {!WebGLShader} vertexShader A vertex shader.
+ * @param {!WebGLShader} fragmentShader A fragment shader.
+ * @return {!WebGLProgram} A program.
+ */
+function createProgram(gl, vertexShader, fragmentShader) {
+  // create a program.
+  var program = gl.createProgram();
+ 
+  // attach the shaders.
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+ 
+  // link the program.
+  gl.linkProgram(program);
+ 
+  // Check if it linked.
+  var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (!success) {
+      // something went wrong with the link
+      throw ("program filed to link:" + gl.getProgramInfoLog (program));
+  }
+ 
+  return program;
+};
+
+/**
+ * Resizes the canvas to be the same size as web browser
+ *
+ * @param {!HTMLCanvas} the canvas to be drawn on.
+ */
+
+function resize(canvas) {
+    // Resize the canvas to be the same size as the browser
+    var displayWidth = canvas.clientWidth;
+    var displayHeight = canvas.clientHeight;
+    
+    console.log("dWidth: " + displayWidth + " dHeight: " + displayHeight);
+    console.log("cWidth: " + canvas.width + " cHeight: " + canvas.height); 
+
+    // check if canvas is the same size
+    if (canvas.width != displayWidth || 
+        canvas.height != displayHeight) {
+        canvas.width = displayWidth;
+        canvas.height = displayHeight;
+        console.log("Canvas Resized")
+        console.log("cWidth: " + canvas.width + " cHeight: " + canvas.height);
+
+    }
+}
